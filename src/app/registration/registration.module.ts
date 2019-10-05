c3b69af64b51378844579ad782d91c5e8ac4a8f6
Noah Lubko <nlubko@gmail.com>
2019-10-05 13:05:50 +0300

    format ts

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+
+import { SignInComponent } from '@registration/sign-in/sign-in.component';
+import { SignUpComponent } from '@registration/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [SignInComponent, SignUpComponent],
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [
     CommonModule
   ]
